5d9f24a9eaabcf6364e35a5085112bd987c4a7dc
MigoCode <micagel22gmail.com>
2022-11-09 14:16:33 +0200

    baseTest added

---
+++ src/main/java/com/herokuapp/theInternet/base/BaseTest.java
@@ -0,0 +1,44 @@
+package com.herokuapp.theInternet.base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @Parameters({ "browser" })
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(@Optional("chrome") String browser) {
+        // Create driver
+        System.out.println("Create driver: " + browser);
+
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        System.out.println("Close driver");
+        // Close browser
+        driver.quit();
+    }
+}
--- src/test/java/com/herokuapp/theinternet/loginTets/NegativeLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginTets/NegativeLoginTests.java
@@ -1,4 +1,4 @@
-package com.herokuapp.theinternet;
+package com.herokuapp.theinternet.loginTets;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
