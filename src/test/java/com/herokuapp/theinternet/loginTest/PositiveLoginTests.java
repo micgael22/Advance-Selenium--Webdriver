e2db3a2e1bba144841cc6ddcd9ff4ade58482fff
MigoCode <micagel22gmail.com>
2022-11-15 23:46:10 +0200

    using pageObjects

--- src/test/java/com/herokuapp/theinternet/loginTest/PositiveLoginTests.java
+++ src/test/java/com/herokuapp/theinternet/loginTest/PositiveLoginTests.java
@@ -1,12 +1,14 @@
 package com.herokuapp.theinternet.loginTest;
 
+import org.apache.logging.log4j.Logger;
 import com.herokuapp.theInternet.base.TestUtilities;
+import com.herokuapp.theInternet.pages.LoginPage;
+import com.herokuapp.theInternet.pages.SecureAreaPage;
+import com.herokuapp.theInternet.pages.WelcomePageObject;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
 
 public class PositiveLoginTests extends TestUtilities {
 
@@ -14,40 +16,30 @@ public class PositiveLoginTests extends TestUtilities {
 	public void logInTest() throws InterruptedException {
 		System.out.println("STARTING POSITIVE LOGIN TEST ---------------------------------->");
 
+		log.info("Starting login test");
+
 		// open main page
-		String url = "http://the-internet.herokuapp.com/";
-		driver.get(url);
-		log.info("Main page is opened.");
+		WelcomePageObject welcomeP = new WelcomePageObject(driver, log);
+		welcomeP.openPage();
 
 		// Click on Form Authentication link
-		driver.findElement(By.linkText("Form Authentication")).click();
+		LoginPage loginP = welcomeP.clickFormAuthenticationLink();
 
-		// enter username and password
-		driver.findElement(By.id("username")).sendKeys("tomsmith");
-		driver.findElement(By.id("password")).sendKeys("SuperSecretPassword!");
-
-//		WebDriverWait wait = new WebDriverWait(driver, 10);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-		sleep(3000);
-
-		// push log in button
-		WebElement logInButton = driver.findElement(By.className("radius"));
-		//wait.until(ExpectedConditions.elementToBeClickable(logInButton));
-		logInButton.click();
+		//execute logIn
+		SecureAreaPage secureAreaP = loginP.logIn("tomsmith", "SuperSecretPassword!");
 
 		// verifications
-		// new url
-		String expectedUrl = "http://the-internet.herokuapp.com/secure";
-		Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+		// new page url is expected
+		Assert.assertEquals(secureAreaP.getCurrentUrl(), secureAreaP.getPageUrl());
 
 		// log out button is visible
-		Assert.assertTrue(driver.findElement(By.xpath("//a[@class='button secondary radius']")).isDisplayed(),
-				"logOutButton is not visible.");
+		Assert.assertTrue(secureAreaP.isLogOutButtonVisible(), "LogOut Button is not Visible");
+//		Assert.assertTrue(driver.findElement(By.xpath("//a[@class='button secondary radius']")).isDisplayed(),
+//				"logOutButton is not visible.");
 
 		// Successful log in message
 		String expectedSuccessMessage = "You logged into a secure area!";
-		String actualSuccessMessage = driver.findElement(By.id("flash")).getText();
+		String actualSuccessMessage = secureAreaP.getSuccessMessageText();
 		Assert.assertTrue(actualSuccessMessage.contains(expectedSuccessMessage),
 				"actualSuccessMessage does not contain expectedSuccessMessage\nexpectedSuccessMessage: "
 						+ expectedSuccessMessage + "\nactualSuccessMessage: " + actualSuccessMessage);
