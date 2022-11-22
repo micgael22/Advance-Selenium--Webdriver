f9f7fa6e1d08be931a750d885e97ca72d0e9e241
MigoCode <micagel22gmail.com>
2022-11-22 21:12:45 +0200

    windowsPage update

--- src/main/java/com/herokuapp/theInternet/pages/BasePageObject.java
+++ src/main/java/com/herokuapp/theInternet/pages/BasePageObject.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class BasePageObject {
 
@@ -84,5 +86,29 @@ public class BasePageObject {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.alertIsPresent());
         return driver.switchTo().alert();
+    }
+
+    //Get title of current page
+    public String getCurrentPageTitle() {
+        return driver.getTitle();
+    }
+
+    //Switch to window with title
+    public void switchToWindowWithTitle(String expectedTitle) {
+        // Switching to new window
+        String firstWindow = driver.getWindowHandle();
+
+        Set<String> allWindows = driver.getWindowHandles();
+        Iterator<String> windowsIterator = allWindows.iterator();
+
+        while (windowsIterator.hasNext()) {
+            String windowHandle = windowsIterator.next().toString();
+            if (!windowHandle.equals(firstWindow)) {
+                driver.switchTo().window(windowHandle);
+                if (getCurrentPageTitle().equals(expectedTitle)) {
+                    break;
+                }
+            }
+        }
     }
 }
