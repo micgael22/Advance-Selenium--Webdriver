78c1d7468a7c9aafbb816c139064219dc3a94604
MigoCode <micagel22gmail.com>
2022-11-24 13:25:01 +0200

    uploadTest commit

---
+++ src/test/java/com/herokuapp/theinternet/fileUploaderTests/UploadTest.java
@@ -0,0 +1,33 @@
+package com.herokuapp.theinternet.fileUploaderTests;
+
+import com.herokuapp.theInternet.base.TestUtilities;
+import com.herokuapp.theInternet.pages.FileUploaderPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UploadTest extends TestUtilities {
+
+    @Test
+    public void imageUploadTest() {
+        log.info("Starting imageUploadTest");
+
+        // open File Uploader Page
+        FileUploaderPage fileUploaderPage = new FileUploaderPage(driver, log);
+        fileUploaderPage.openPage();
+
+        // Select file
+        String fileName = "logo.png";
+        fileUploaderPage.selectFile(fileName);
+
+        // Push upload button
+        fileUploaderPage.pushUploadButton();
+
+        // Get uploaded files
+        String fileNames = fileUploaderPage.getUploadedFilesNames();
+
+        // Verify selected file uploaded
+        Assert.assertTrue(fileNames.contains(fileName),
+                "Our file (" + fileName + ") is not one of the uploaded (" + fileNames + ")");
+    }
+
+}
