43595f01ed9a93cda355b9a710fc0a86bfb6a5e9
MigoCode <micagel22gmail.com>
2022-12-01 12:34:31 +0200

    parameters&methods added

--- src/main/java/com/herokuapp/theInternet/base/BaseTest.java
+++ src/main/java/com/herokuapp/theInternet/base/BaseTest.java
@@ -16,19 +16,21 @@ public class BaseTest {
     protected String testName;
     protected String testMethodName;
 
-    @Parameters({ "browser", "chromeProfile" })
+    @Parameters({ "browser", "chromeProfile", "deviceName" })
     @BeforeMethod(alwaysRun = true)
-    public void setUp(Method method, @Optional("chrome") String browser, @Optional String profile, ITestContext ctx) {
+    public void setUp(Method method, @Optional("chrome") String browser, @Optional String profile,
+                      @Optional String deviceName, ITestContext ctx) {
 
         System.out.println("[---------ON SETUP ---------]");
 
         String testName = ctx.getCurrentXmlTest().getName();
         log = LogManager.getLogger(testName);
 
-        BrowserDriverFactory factory = new BrowserDriverFactory(browser ,log);
-
+        BrowserDriverFactory factory = new BrowserDriverFactory(browser, log);
         if (profile != null) {
             driver = factory.createChromeWithProfile(profile);
+        } else if (deviceName != null) {
+            driver = factory.createChromeWithMobileEmulation(deviceName);
         } else {
             driver = factory.createDriver();
         }
